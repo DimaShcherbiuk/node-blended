bf2fc7edba3e73b083b0c2701df6b21811713ffb
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-07-13 15:54:48 +0300

    some things for 3 hw done

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 
 import { env } from "./utils/env.js";
 import { getAllProducts, getProductById } from "./controllers/products.js";
+import { ctrlWrapper } from "./utils/ctrlWrapper.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const PORT = Number(env("PORT", "8000"));
 
@@ -12,9 +15,13 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
 
-  app.get('/products', getAllProducts)
+  app.get('/products', ctrlWrapper(getAllProducts));
 
-  app.get('/products/:productId', getProductById)
+  app.get('/products/:productId', ctrlWrapper(getProductById));
+
+  app.use("*", notFoundHandler);
+  
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
